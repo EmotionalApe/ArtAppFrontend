4d0e930c338f3b2a55a02d1e972f80cc1aadc0a8
Maruf Khan <maruf1611khan@gmail.com>
2023-11-19 19:21:26 +0530

    router, homepage, navbar add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import HomePage from './pages/HomePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      Working
+    <div >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const HomePage = () => {
+  return (
+    <div>
+        HomePage
+        <Navbar/>
+    </div>
+  )
+}
+
+export default HomePage
